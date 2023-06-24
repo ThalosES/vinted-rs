3ae3e061673b24e4b0f19ff5ea7ad15d90b6a98b
pxp9 <pepe.marquezromero@gmail.com>
2023-06-24 19:25:40 +0200

    cookie obtenida

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use vinted_rs::queries;
+use regex::bytes::Regex;
 
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
+let Ok(regex) = Regex::new(r"cf_bm=([^;]+)") else{
+    panic!("")
+};
+let cookie = queries::refresh_cookie(regex).await.unwrap();
 
+println!("Cookie : {}" , cookie);
 }
---
+++ src/queries.rs
@@ -0,0 +1,83 @@
+use regex::bytes::Regex;
+// use serde::{Deserialize, Serialize};
+use std::str::from_utf8;
+use std::str::Utf8Error;
+use thiserror::Error;
+
+
+/*
+"https://www.vinted.fr/auth/token_refresh"
+"https://www.vinted.be/auth/token_refresh"
+"https://www.vinted.es/auth/token_refresh"
+"https://www.vinted.lu/auth/token_refresh"
+"https://www.vinted.nl/auth/token_refresh"
+"https://www.vinted.lt/auth/token_refresh"
+"https://www.vinted.de/auth/token_refresh"
+"https://www.vinted.at/auth/token_refresh"
+"https://www.vinted.it/auth/token_refresh"
+"https://www.vinted.co.uk/auth/token_refresh"
+"https://www.vinted.pt/auth/token_refresh"
+"https://www.vinted.com/auth/token_refresh"
+"https://www.vinted.cz/auth/token_refresh"
+"https://www.vinted.sk/auth/token_refresh"
+"https://www.vinted.pl/auth/token_refresh"
+"https://www.vinted.se/auth/token_refresh"
+"https://www.vinted.ro/auth/token_refresh"
+"https://www.vinted.hu/auth/token_refresh"
+*/
+
+
+
+#[derive(Error, Debug)]
+pub enum CookieError {
+    #[error("ReqwestError")]
+    ReqWestError(#[from] reqwest::Error),
+    #[error("Utf8Error")]
+    Utf8Error(#[from] Utf8Error),
+    #[error("NoLastElement")]
+    NoLastElement,
+    #[error("NoCapturesError")]
+    NoCaptures,
+    #[error("NoMatchingError")]
+    NoMatching,
+}
+
+pub async fn refresh_cookie(re: Regex) -> Result<String, CookieError> {
+    let client = reqwest::Client::new();
+    let host = "https://www.vinted.at";
+    let request = format!("{host}/auth/token_refresh");
+    let res = client
+        .post(request)
+        // .header("User-Agent", "PostmanRuntime/7.32.3")
+        // .header("Host",  host)
+        // .header("Connection", "keep-alive")
+        // .header("Accept", "*/*")
+        // .header("Accept-Encoding", "gzip, deflate, br")
+        .send()
+        .await?;
+
+    let headers = res.headers();
+    /*     .json()
+        .await?;
+    */
+    let get_all = headers.get_all("set-cookie");
+
+    let Some(cookie_to_parse) = get_all.into_iter().last() else {
+        return Err(CookieError::NoLastElement);
+    };
+
+    let cook = cookie_to_parse.as_bytes();
+
+    let Some(captures) = re.captures(cook) else{
+        return Err(CookieError::NoCaptures);
+        };
+    let Some(cf_bm_value_bytes) = captures.get(1) else {
+        return Err(CookieError::NoMatching);
+        };
+
+    let cf_bm_value : String = from_utf8(cf_bm_value_bytes.as_bytes())?.to_string();
+
+    Ok(cf_bm_value)
+}
+
+pub async fn get_item() {}
