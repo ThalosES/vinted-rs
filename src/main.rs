9ff7f8e49668a5fa549b395ba5c4c224a9677d03
pxp9 <pepe.marquezromero@gmail.com>
2023-06-25 20:14:43 +0200

    fix format

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use vinted_rs::queries;
 
 #[tokio::main]
 async fn main() {
-
     let mut vinted = queries::VintedWrapper::new();
 
     vinted.refresh_cookies().await.unwrap();
@@ -20,5 +19,4 @@ async fn main() {
 
     let _items: Items = Items::new(items);
     */
-
 }
