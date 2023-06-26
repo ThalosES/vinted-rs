e0a769f7517cad1325ca51a93252356844b740e8
alvarocabo <alvarocaboac2@gmail.com>
2023-06-26 13:36:04 +0200

    Defining item schema

--- src/model/item.rs
+++ src/model/item.rs
@@ -1,3 +1,12 @@
 use crate::model::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Item {}
+pub struct Item {
+    id: i32,
+    title: String,
+    price: i64,
+    brand: String,
+    color: String,
+    size: String,
+    photo_url: String,
+    
+}
