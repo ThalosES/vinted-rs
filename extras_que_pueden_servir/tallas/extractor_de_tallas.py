997bb8e52148ef15d5a04f743712062f8d5c86f1
pxp9 <pepe.marquezromero@gmail.com>
2023-06-28 14:40:02 +0200

    arreglillos de ultima hora

--- extras_que_pueden_servir/tallas/extractor_de_tallas.py
+++ extras_que_pueden_servir/tallas/extractor_de_tallas.py
@@ -10,7 +10,7 @@ def obtener(path):
     # Parsear el contenido HTML con BeautifulSoup
     soup = BeautifulSoup(contenido_html, 'html.parser')
     div = soup.find_all('div', attrs={'class': 'web_ui__Navigation__body'})
-    print('New:' + div[0].text)
+    categoria = div[0].text
     pattern = re.compile(r'size_group_sizes_(\d+)-list-item-(\d+)')
         
     # Encontrar todas las etiquetas label con el atributo for igual a 'size_group_sizes_4_'
@@ -24,36 +24,36 @@ def obtener(path):
             numeros.append((numero , valor_h2))
     # Agregar las etiquetas encontradas a la lista
 
-    return numeros
+    return (numeros , categoria)
 
-def print_result(lista):
+def print_result(lista , categoria):
     for num, val in lista:
-        print(num + " , " + val)
+        print(num + " , " + val + " , "+ categoria)
 # Ejemplo de uso
 path = 'mascotas.html'  # Reemplaza con la URL de la página web que deseas analizar
 
-numeros = obtener(path)
-print_result(numeros)
+numeros , categoria = obtener(path)
+print_result(numeros , categoria)
 
 path = 'hogar.html'  # Reemplaza con la URL de la página web que deseas analizar
 
-numeros = obtener(path)
-print_result(numeros)
+numeros , categoria = obtener(path)
+print_result(numeros , categoria)
 
 path = 'hombre.html'  # Reemplaza con la URL de la página web que deseas analizar
 
-numeros = obtener(path)
-print_result(numeros)
+numeros , categoria = obtener(path)
+print_result(numeros, categoria)
 
 path = 'mujer.html'  # Reemplaza con la URL de la página web que deseas analizar
 
-numeros = obtener(path)
-print_result(numeros)
+numeros , categoria = obtener(path)
+print_result(numeros, categoria)
 
 
 path = 'niños.html'  # Reemplaza con la URL de la página web que deseas analizar
 
-numeros = obtener(path)
-print_result(numeros)
+numeros , categoria = obtener(path)
+print_result(numeros , categoria)
 
 
