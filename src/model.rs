0221334e61845746509c5d203274db82179cbba5
Alvaro Cabo <74014545+alvarocabo@users.noreply.github.com>
2023-07-08 22:25:50 +0200

    Project documented (#23)
    * Pxp9/ft filters url (#14)
    
    * tenemos las tallas gracias a la cortesía de ChatGPT beautiful soup y una buena feature del navegador xd, subimos el JSON donde están todas las categorías (falta parsear) y empezamos con las marcas
    
    * script de python que te saca todos los nombres de marcas de la pagina brands/byLetter/Letra
    
    * nombres de marcas y tallas extraidas
    
    * script que hace requests a la api y obtiene el ID de la marca
    
    * Got brands_ids :D
    
    * formatting catalogs ids
    
    * organizando los recursos de los filtros
    
    * materiales
    
    * arreglillos de ultima hora
    
    * arreglillos de ultima hora
    
    * Added scrappers as submodule
    
    * Refactor completed
    
    * Fmt formatted
    
    * actualizando el submodulo del scrapper
    
    * primera migration sizes complete
    
    * get_catalogs , alvaro y README
    
    * Finished catalogs
    
    * Fix README
    
    * Fix both Readme
    
    * Fix dependency for Diesel
    
    * Created more migrations
    
    * Brands needs a fix
    
    * fix migrations and brands
    
    * migracion de countries y query de la BBDD de una marca
    
    * fix clippy
    
    * arreglando un bug con las cookies, haciendo un test y limitando get_item a 1 elemento
    
    * fix fmt
    
    * mejorando la API del wrapper , implementando FROM de Brand
    
    * fix fmt
    
    * fix clippy on test
    
    * review API and model types
    
    * fix fmt
    
    * Db tests
    
    * fix workflow
    
    * empezando a procesar los filtros en la URI
    
    * Finished db-feeder
    
    * Added some tests
    
    * Added query
    
    * Added optional num field in get_items
    
    * Added Get_category_by_name tested
    
    * Converted vec<i32> to String
    
    Co-authored-by: Pmarquez <pxp9@users.noreply.github.com>
    
    * Finished filters
    
    * Fix doctest
    
    * Added get-host
    
    * Cargo clippy fix
    
    * Small changes big steps
    
    * Fix languages
    
    * fuck cargo fmt
    
    ---------
    
    Co-authored-by: alvarocabo <alvarocaboac2@gmail.com>
    Co-authored-by: Pmarquez <pxp9@users.noreply.github.com>
    
    * Fix cargo lock
    
    * Add VintedWrapperError
    
    * Added badges
    
    * Fix doc tests
    
    * Documented
    
    * Documented model
    
    * Fix unnecesary info
    
    * Of course clippy
    
    * Documented db module
    
    * Improved db module docs
    
    * Small fix
    
    * Documented filters
    
    * Cargo fmt
    
    ---------
    
    Co-authored-by: Pmarquez <48651252+pxp9@users.noreply.github.com>
    Co-authored-by: Pmarquez <pxp9@users.noreply.github.com>

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,73 @@
+/*!
+ * The `model` module provides abstraction and serialization for the request and result
+ * objects of the Vinted API queries.
+ */
+
+/// The `filter` module provides structures and enums for filtering items in the Vinted API.
+///
+/// It contains the following modules:
+/// - `brand`: Provides functionality related to filtering by brand.
+/// - `category`: Provides functionality related to filtering by category.
+/// - `category_tree`: Provides functionality for retrieving the category tree.
+/// - `colors`: Provides functionality related to filtering by color.
+/// - `country`: Provides functionality related to filtering by country.
+/// - `material`: Provides functionality related to filtering by material.
+/// - `size`: Provides functionality related to filtering by size.
 pub mod filter;
+
+/// The `item` module provides the `Item` struct representing an item in the Vinted API.
+///
+/// It depends on the `photo` module for handling item photos.
+///
+/// # Structs
+///
+/// - `Item`: Represents an item in the Vinted API.
+///    - `id`: The ID of the item.
+///    - `title`: The title of the item.
+///    - `size_title`: The size title of the item.
+///    - `brand_title`: The brand title of the item.
+///    - `currency`: The currency used for the item price.
+///    - `price`: The price of the item.
+///    - `photo`: The photo of the item.
+///    - `url`: The URL of the item.
+///    - `is_visible`: A flag indicating if the item is visible.
+///    - `promoted`: A flag indicating if the item is promoted.
+///    - `favourite_count`: The count of favorites for the item.
 pub mod item;
+
+/// The `items` module provides the `Items` struct representing a collection of items in the Vinted API.
+///
+/// It depends on the `item` module for representing individual items.
+///
+/// # Structs
+///
+/// - `Items`: Represents a collection of items in the Vinted API.
+///    - `items`: The list of items.
+///
+/// # Methods
+///
+/// - `new(items: Vec<Item>) -> Self`: Creates a new instance of `Items` with the provided list of items.
 pub mod items;
+
+/// The `photo` module provides the `Photo` struct representing a photo in the Vinted API.
+///
+/// # Structs
+///
+/// - `Photo`: Represents a photo in the Vinted API.
+///    - `id`: The ID of the photo.
+///    - `url`: The URL of the photo.
+///    - `dominant_color`: The dominant color of the photo.
+///    - `dominant_color_opaque`: The opaque dominant color of the photo.
 pub mod photo;
+
+/// The `User` struct represents a user in the Vinted API.
+///
+/// It depends on the `photo` module for handling user photos.
+///
+/// # Struct Fields
+///
+/// - `id`: The ID of the user.
+/// - `login`: The username of the user.
+/// - `photo`: The photo of the user.
 pub mod user;
 pub use serde::{Deserialize, Serialize};
