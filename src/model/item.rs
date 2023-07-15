05da33763b86a3b758a58bfb36c8225565cbc8f2
alvarocabo <alvarocaboac2@gmail.com>
2023-07-15 13:33:11 +0200

    ChatGPT generated

--- src/model/item.rs
+++ src/model/item.rs
@@ -1,5 +1,5 @@
 use crate::model::{Deserialize, Serialize};
-
+use std::fmt;
 use super::photo::Photo;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -15,4 +15,23 @@ pub struct Item {
     pub is_visible: i32,
     pub promoted: bool,
     pub favourite_count: i32,
+
+}
+
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "ID: {}", self.id)?;
+        writeln!(f, "Title: {}", self.title)?;
+        writeln!(f, "Size Title: {}", self.size_title)?;
+        writeln!(f, "Brand Title: {}", self.brand_title)?;
+        writeln!(f, "Currency: {}", self.currency)?;
+        writeln!(f, "Price: {}", self.price)?;
+        writeln!(f, "Photo: {}", self.photo)?;
+        writeln!(f, "URL: {}", self.url)?;
+        writeln!(f, "Is Visible: {}", self.is_visible)?;
+        writeln!(f, "Promoted: {}", self.promoted)?;
+        writeln!(f, "Favourite Count: {}", self.favourite_count)?;
+
+        Ok(())
+    }
 }
--- src/model/items.rs
+++ src/model/items.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::model::item::Item;
 use crate::model::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
@@ -8,5 +10,17 @@ pub struct Items {
 impl Items {
     pub fn new(items: Vec<Item>) -> Self {
         Items { items }
+    }
+}
+
+
+impl fmt::Display for Items {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for item in &self.items {
+            writeln!(f, "{}", item)?;
+            writeln!(f, "----------------------")?;
+        }
+
+        Ok(())
     }
 }
