83fb0e484693cd097d71c8384aa3be2391edffff
alvarocabo <alvarocaboac2@gmail.com>
2023-07-15 14:13:01 +0200

    Implemented visualize picture photo

--- src/model/photo.rs
+++ src/model/photo.rs
@@ -10,6 +10,31 @@ pub struct Photo {
     pub dominant_color_opaque: String,
 }
 
+impl Photo {
+    pub async fn visualize_picture(&self) -> Result<String, reqwest::Error> {
+        if self.url.is_empty() {
+            return Ok("No image URL provided".to_string());
+        }
+
+        let response = reqwest::get(&self.url).await?;
+        if !response.status().is_success() {
+            return Ok("Failed to retrieve image".to_string());
+        }
+
+        let content_type = response
+            .headers()
+            .get("content-type")
+            .and_then(|ct| ct.to_str().ok())
+            .unwrap_or("");
+
+        if content_type.starts_with("image/") {
+            Ok(format!("Image: [{}]", self.url))
+        } else {
+            Ok("Invalid image URL".to_string())
+        }
+    }
+}
+
 impl fmt::Display for Photo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "ID: {}", self.id)?;
