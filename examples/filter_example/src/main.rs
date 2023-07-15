276687f7ac0397828120ea26a0273d66891c821f
pxp9 <pepe.marquezromero@gmail.com>
2023-07-15 11:56:46 +0200

    example filter

---
+++ examples/filter_example/src/main.rs
@@ -0,0 +1,26 @@
+use bb8_postgres::tokio_postgres::NoTls;
+use vinted_rs::{db::DbController, Filter, VintedWrapper};
+
+#[tokio::main]
+async fn main() {
+    let db = DbController::new("postgres://postgres:postgres@localhost/vinted-rs", 5, NoTls)
+        .await
+        .unwrap();
+
+    let adidas = db.get_brand_by_name(&"Adidas").await.unwrap();
+    let nike = db.get_brand_by_name(&"Nike").await.unwrap();
+
+    let brands = format!("{},{}", adidas.id, nike.id);
+
+    let filter = Filter::builder()
+        .brand_ids(brands)
+        .price_from(15)
+        .price_to(20)
+        .build();
+
+    let vinted = VintedWrapper::new();
+
+    let items = vinted.get_items(&filter, 10).await.unwrap();
+
+    println!("{:?}", items);
+}
