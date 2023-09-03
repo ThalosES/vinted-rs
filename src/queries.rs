66b5177e3fb5b58b4331dbedabcc337a8b5fcfdc
pxp9 <pepe.marquezromero@gmail.com>
2023-09-03 18:47:11 +0200

    fix cookies removal

--- src/queries.rs
+++ src/queries.rs
@@ -486,7 +486,10 @@ impl<'a> VintedWrapper<'a> {
         user_agent: Option<&str>,
         proxy: Option<Proxy>,
     ) -> Result<(), CookieError> {
-        self.cookie_store.lock().unwrap().clear();
+        self.cookie_store
+            .lock()
+            .unwrap()
+            .remove(&format!("vinted.{}", self.host), "/", "__cf_bm");
 
         let client = self.get_client(user_agent, proxy);
 
